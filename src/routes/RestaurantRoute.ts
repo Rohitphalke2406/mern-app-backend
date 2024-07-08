dc50a605a7c28105c7664f2d6094047e884e16bb
Rohit Phalke <rohitphalke165@gmail.com>
2024-07-08 19:33:46 +0530

    checkout item added

--- src/routes/RestaurantRoute.ts
+++ src/routes/RestaurantRoute.ts
@@ -4,6 +4,15 @@ import RestaurantController from "../controllers/RestaurantController";
 
 const router = express.Router();
 
+router.get(
+    "/:restaurantId",
+    param("restaurantId")
+      .isString()
+      .trim()
+      .notEmpty()
+      .withMessage("RestaurantId paramenter must be a valid string"),
+    RestaurantController.getRestaurant
+  );
 router.get("/search/:city" , param("city").isString().trim().notEmpty().withMessage("city Parameter must be valid"), RestaurantController.searchRestaurant);
 
 export default router;
